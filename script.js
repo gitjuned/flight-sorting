02fa1b80a5e526925ce8bb13d0734c369b8acbf8
gitjuned <junaydk46@gmail.com>
2019-06-06 16:38:52 +0530

    Filtered by name, Displaying results on DOM

--- script.js
+++ script.js
@@ -21,17 +21,106 @@ var data =
     {
         name: "Jet Airways",
         duration: "21hour10min",
-        price: 3400,
+        price: 6400,
 
     },
 
 ]
 
-console.log("Array of Overall Data");
-console.log(data);
+// console.log("Array of Overall Data");
+// console.log(data);
 
 
+var tempArray = [];
+var totalCheckedFlights = [];
+var totalUnCheckedFlights = [];
+var airlineSelected = [];
+var checkedFlight;
+var filteredData=[];
 
+$('input:checkbox').click(function(){
+        $("#result").html("");
+        $("input:checkbox").each(function(){
+
+            if ($(this).prop('checked') == true){
+                checkedFlight = $(this).val();
+
+                filteredData = data.filter(function (item) {
+                    return item.name == checkedFlight;
+                });
+                console.log("****** FILTERED");
+                console.log(filteredData);
+                // $("#result").append("hey there");
+                $("#result").html("");
+                for(let i=0;i<filteredData.length;i++){
+                    
+                    $("#result").append(
+
+                        '<div class="row">'+
+
+                            '<div class="col-sm-4"> '+
+                                '<div class="flight-name">'+
+                                    '<p>'+filteredData[i].name+'</p>'+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="col-sm-4"> '+
+                                '<div class="flight-duration">'+
+                                    '<p>'+filteredData[i].duration+'</p>'+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="col-sm-4"> '+
+                                '<div class="flight-price">'+
+                                    '<p>'+filteredData[i].price+'</p>'+
+                                '</div>'+
+                            '</div>'+
+
+                        '</div>'    
+                    )
+                }
+
+            }
+            else{
+                var filteredData = data.filter(function (item) {
+                    return item;
+                });
+                console.log("oops");
+                
+            }
+        }); 
+});
+
+
+function appendData(){
+    for(let i=0;i<data.length;i++){
+
+        $("#result").append(
+
+            '<div class="row">'+
+
+                '<div class="col-sm-4"> '+
+                    '<div class="flight-name">'+
+                        '<p>'+data[i].name+'</p>'+
+                    '</div>'+
+                '</div>'+
+                '<div class="col-sm-4"> '+
+                    '<div class="flight-duration">'+
+                        '<p>'+data[i].duration+'</p>'+
+                    '</div>'+
+                '</div>'+
+                '<div class="col-sm-4"> '+
+                    '<div class="flight-price">'+
+                        '<p>'+data[i].price+'</p>'+
+                    '</div>'+
+                '</div>'+
+
+            '</div>'    
+        )
+    }
+}
+
+
+appendData();
+ 
 var jetFlights = data.filter(item => item.name == "Jet Airways");
 console.log("Jet Airways Flights");
 console.log(jetFlights);
@@ -46,6 +135,6 @@ console.log(indigoFlights);
 
 
 // var jet2 = data.filter(function (item) {
-// 	return item.name == "jet";
+// 	return item.name == "Jet Airways";
 // });
 // console.log(jet2);
